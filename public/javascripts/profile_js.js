f31e3de9073fcc700d3a659a54355cb88c031e6f
Amanda Vazquez <amanda.vazquez2@upr.edu>
2020-05-07 22:31:56 -0400

    added profile html base code

---
+++ public/javascripts/profile_js.js
@@ -0,0 +1,8 @@
+function updatePhoto() {
+    var x = document.getElementById("photoSubmit");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
