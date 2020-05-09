37a6b9bef91851aab72c57280825408e15987f25
J.D <juan.ramos15@upr.edu>
2020-05-08 21:30:55 -0400

    Stuck in error, still debugging

--- app/controllers/ProfileController.scala
+++ app/controllers/ProfileController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.nio.file.Paths
+
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -16,6 +18,23 @@ class ProfileController @Inject()(val controllerComponents: ControllerComponents
    */
   def profile() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.profile())
+  }
+
+  def upload = Action(parse.multipartFormData) { request =>
+    request.body
+      .file("picture")
+      .map { picture =>
+        // only get the last part of the filename
+        // otherwise someone can send a path like ../../home/foo/bar.txt to write to other files on the system
+        val filename    = Paths.get(picture.filename).getFileName
+        val fileSize    = picture.fileSize
+        val contentType = picture.contentType
+        picture.ref.copyTo(Paths.get(s"/public/images/$filename"), replace = true)
+        Ok("File uploaded")
+      }
+      .getOrElse {
+        Ok("ERROR404")
+      }
   }
 
 }
